53c1622ad6e32044737adbb2e37d9289f89ebc06
pokotsun <gorigolila@gmail.com>
2020-08-01 21:53:05 +0900

    init cargo and create actix_web template

---
+++ webapp/rust/src/main.rs
@@ -0,0 +1,29 @@
+extern crate actix_web;
+
+use std::{env};
+
+use actix_session::{CookieSession};
+use actix_web::{
+    middleware, web, App, HttpRequest, HttpResponse, HttpServer,
+};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(CookieSession::signed(&[0; 32]).secure(false))
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").route(web::get().to(|req: HttpRequest| {
+                println!("{:?}", req);
+                HttpResponse::Ok()
+                    .content_type("text/plain")
+                    .body("Hello, actix_web.")
+            })))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
