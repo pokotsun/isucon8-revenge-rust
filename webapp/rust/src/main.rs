c4d08d5a4e59c2104a822ba02c359eef4f0fa842
pokotsun <gorigolila@gmail.com>
2020-08-01 23:38:50 +0900

    define struct Event, Sheets, Sheet

--- webapp/rust/src/main.rs
+++ webapp/rust/src/main.rs
@@ -1,15 +1,50 @@
 extern crate actix_web;
 
+use std::collections::HashMap;
 use std::env;
 
 use actix_session::CookieSession;
 use actix_web::{middleware, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+use chrono::NaiveDateTime;
 
 struct User {
     id: i64,
     nick_name: String,
     login_name: String,
     pass_hash: String,
+}
+
+// TODO db_column nameとの対応
+struct Event {
+    id: i64,
+    title: String,
+    public: bool,
+    closed: bool,
+    price: i64,
+
+    total: i32,
+    remains: i32,
+    sheets: HashMap<String, Sheets>,
+}
+
+struct Sheets {
+    total: i32,
+    remains: i32,
+    details: Vec<Sheet>,
+    price: i64,
+}
+
+struct Sheet {
+    id: i64,
+    rank: String,
+    num: i64,
+    price: i64,
+
+    mine: bool,
+    reserved: bool,
+    reserved_at: NaiveDateTime,
+    reserved_at_unix: i64,
 }
 
 async fn get_dummy(req: HttpRequest) -> impl Responder {
